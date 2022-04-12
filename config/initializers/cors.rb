9f4c509c171980eb523471ffde86f866889d56c5
watarunakanishi190647 <95616173+watarunakanishi190647@users.noreply.github.com>
2022-04-12 23:00:48 +0900

    cors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '127.0.0.1:3000', 'localhost:3000','https://twitter-wheat.vercel.app/'
-  
+      origins 'https://twitter-wheat.vercel.app/'
+      # '127.0.0.1:3000', 'localhost:3000',
+      
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
