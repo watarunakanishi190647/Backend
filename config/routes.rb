0f23dd27047262ed9b4c362bcda775289ae6472b
watarunakanishi190647 <95616173+watarunakanishi190647@users.noreply.github.com>
2022-04-13 13:37:21 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
+  get "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
