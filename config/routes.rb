c7baa82edba093dc79d724eef5cfcf3023cd0b66
watarunakanishi190647 <95616173+watarunakanishi190647@users.noreply.github.com>
2022-04-13 14:24:03 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
 
-  post "/graphql", to: "graphql#execute"
+  post to: "graphql#execute"
+  # post "/graphql", to: "graphql#execute"
+
   # get "/graphql", to: "graphql#execute"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
