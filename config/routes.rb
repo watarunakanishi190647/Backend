1696e4f223efb58485d32bc47fc20ca83ecbdef1
watarunakanishi190647 <95616173+watarunakanishi190647@users.noreply.github.com>
2022-04-13 14:27:00 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
 
-  post to: "graphql#execute"
+  post "still-ravine-42221.herokuapp.com", to: "graphql#execute"
   # post "/graphql", to: "graphql#execute"
 
   # get "/graphql", to: "graphql#execute"
